36628459259a7e267ad6fb7b73f7ea20edb4d84e
nthhuy <50795306+nthhuy@users.noreply.github.com>
2019-05-19 18:49:40 +0700

    abc

--- bai1.cpp
+++ bai1.cpp
@@ -1,31 +1,40 @@
 #include<iostream>
 
 using namespace std;
+
+void inSert(int m[],int n){
+    for(int i=0; i<n; i++)
+        cin >> m[i];
+}
+void print(int m[], int n){
+    for(int i=0; i<n; i++)
+        cout << m[i];
+}
+
 int main() {
     int n, sum=0, j;
     cin >> n;
-    int mang[n];
+    int _mang[n];
     int i=0;
     bool swapped;
-    do{
-        cin >> mang[i++];
 
-    } while(i<n);
+    inSert(_mang,n);
+//    do{
+//        cin >> mang[i++];
+//
+//    } while(i<n);
 
     do{
         swapped = false;
         for(i=0; i<n-1; i++) {
-            if(mang[i]>mang[i+1]){
-                swap(mang[i],mang[i+1]);
+            if(_mang[i]>_mang[i+1]){
+                swap(_mang[i],_mang[i+1]);
                 swapped=true;
             }
         }
      } while (swapped);
 
-    for(int i=0; i<n; i++){
-
-        cout << mang[i];
-    }
+    print(_mang, n);
 
 
 
